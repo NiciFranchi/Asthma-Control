2d8ddb09edd0d5f33d3c333cedbd65350e1d9509
atalati6 <atalati6@gatech.edu>
2018-03-24 01:29:50 -0400

    useless config change

---
+++ src/main/java/edu/gatech/epidemics/SpringBootConfig.java
@@ -0,0 +1,21 @@
+package edu.gatech.epidemics;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author atalati
+ */
+@Configuration
+@EnableWebMvc
+public class SpringBootConfig implements WebMvcConfigurer
+{
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer)
+	{
+		configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+	}
+}
