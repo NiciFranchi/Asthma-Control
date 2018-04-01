18c8a75b11bd8a8ae27252cc5b434fd36a40156c
eleschinski3 <eleschinski3>
2018-04-01 16:48:54 -0400

    Adding schema files

--- database/01_schema.sql
+++ database/01_schema.sql
@@ -8,6 +8,74 @@ DROP TABLE IF EXISTS login;
 CREATE TABLE login (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `username` VARCHAR(25) NOT NULL,
-`password` VARCHAR(50) ,
+`password` VARCHAR(50),
 `active` BIT(1)
+)ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS person;
+
+CREATE TABLE person (
+`id` INT NOT NULL PRIMARY KEY,
+`firstName` VARCHAR(50) NOT NULL,
+`middleName` VARCHAR(50) NULL,
+`lastName` VARCHAR(50) NOT NULL,
+`contactPhone` VARCHAR(50) NULL,
+`contactEmail` VARCHAR(50) NULL,
+`contactFax` VARCHAR(50) NULL,
+`addressLine1` VARCHAR(100) NULL,
+`addressLine2` VARCHAR(100) NULL,
+`city` VARCHAR(50) NULL,
+`state` VARCHAR(50) NULL,
+`zip` VARCHAR(10) NULL
+)ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS patient;
+
+CREATE TABLE patient (
+`id` INT NOT NULL PRIMARY KEY,
+`personId` INT NOT NULL REFERENCES person(id),
+`ssn` VARCHAR(25) NOT NULL,
+`birthday` DATE NOT NULL # This will store just a day without a time.
+)ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS doctor;
+
+CREATE TABLE doctor (
+`id` INT NOT NULL PRIMARY KEY,
+`personId` INT NOT NULL REFERENCES person(id),
+`employeeId` VARCHAR(25) NOT NULL,
+`specialty` VARCHAR(25) NOT NULL,
+`birthday` DATE NOT NULL
+)ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS questionnaire;
+
+CREATE TABLE questionnaire (
+`id` INT NOT NULL PRIMARY KEY,
+`ageGroup` ENUM('ZeroToFour', 'FiveToEleven', 'TwelvePlus') NOT NULL,
+`questionNumber` INT NOT NULL,
+`question` VARCHAR(100) NOT NULL,
+`answerList` INT NOT NULL REFERENCES answerChoice(listId),
+`domainOfControl` ENUM('Impairment', 'Risk') NOT NULL
+)ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS answerChoice;
+
+CREATE TABLE answerChoice (
+`id` INT NOT NULL PRIMARY KEY,
+`listId` INT NOT NULL,
+`answerNumber` INT NOT NULL,
+`description` VARCHAR(100) NOT NULL,
+INDEX answerListChoice (listId, answerNumber)
+)ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS visit;
+
+CREATE TABLE visit (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`patientId` INT NOT NULL REFERENCES patient(id),
+`doctorId` INT NOT NULL REFERENCES doctor(id),
+`questionnaireId` INT NOT NULL REFERENCES questionnaire(id),
+`answerId` INT NOT NULL REFERENCES answerChoice(id),
+`visitDate` DATETIME NOT NULL
 )ENGINE=InnoDB;
--- database/02_data.sql
+++ database/02_data.sql
@@ -15,4 +15,30 @@ INSERT INTO `login` (`username`, `password`, `active`) VALUES ('april', 'beiter'
 INSERT INTO `login` (`username`, `password`, `active`) VALUES ('jordan', 'rimmer', 1);
 INSERT INTO `login` (`username`, `password`, `active`) VALUES ('shanon', 'johnson', 0);
 
+DELETE FROM `person`;
+INSERT INTO `person` (`id`, `firstName`, `lastName`) VALUES(0, 'Lewis', 'Zimmerman');
+INSERT INTO `person` (`id`, `firstName`, `lastName`) VALUES(1, 'Tom', 'Paris');
+
+DELETE FROM `doctor`;
+INSERT INTO `doctor` (`id`, `personId`, `employeeId`, `specialty`, `birthday`) VALUES(0, 0, 'NCC-74656', 'medical hologram', '1995-01-16');
+
+DELETE FROM `patient`;
+INSERT INTO `patient` (`id`, `personId`, `ssn`, `birthday`) VALUES(0, 1, 'NCC-74656-1', '1995-01-16');
+
+DELETE FROM `answerChoice`;
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(0, 1, 1, '<=2 days/week');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(1, 1, 2, '>2 days/week');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(2, 1, 3, 'Throughout the day');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(3, 2, 1, '<= 1x/month');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(4, 2, 2, '> 1x/month');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(5, 2, 3, '>= 1x/week');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(6, 3, 1, '<=2 days /week but no more than once on each day');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(7, 3, 2, '>2 days/week or multiple times on <=2 days /week');
+INSERT INTO `answerChoice` (`id`, `listId`, `answerNumber`, `description`) VALUES(8, 3, 3, 'Throughout the day');
+
+DELETE FROM `questionnaire`;
+INSERT INTO `questionnaire` (`id`, `ageGroup`, `questionNumber`, `question`, `answerList`, `domainOfControl`) VALUES(0, 'ZeroToFour', 1, 'Asthma symptoms in previous 2 - 4 weeks', 1, 'Impairment');
+INSERT INTO `questionnaire` (`id`, `ageGroup`, `questionNumber`, `question`, `answerList`, `domainOfControl`) VALUES(1, 'ZeroToFour', 2, 'Night time awakenings in previous 2 - 4 weeks', 2, 'Impairment');
+INSERT INTO `questionnaire` (`id`, `ageGroup`, `questionNumber`, `question`, `answerList`, `domainOfControl`) VALUES(2, 'FiveToEleven', 1, 'Asthma symptoms in previous 2 - 4 weeks', 3, 'Impairment');
+
 SET SQL_SAFE_UPDATES = 1;
