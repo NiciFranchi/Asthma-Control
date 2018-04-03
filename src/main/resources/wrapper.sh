0a4ddf4927844c558a1c7f4e16ad52fd44b3fbba
atalati6 <atalati6@gatech.edu>
2018-04-02 20:05:16 -0400

    make sure, HAPI FHIR is ready before web app starts

--- src/main/resources/wrapper.sh
+++ src/main/resources/wrapper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+while ! exec 6<>/dev/tcp/${FHIR_HOST}/${FHIR_PORT}; do
+    echo "Trying to reach FHIR server at ${FHIR_HOST}:${FHIR_PORT}..."
+    sleep 5
+done
+echo ">> FHIR server is now REACHABLE! <<"
 while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
     echo "Trying to connect to MySQL at ${DATABASE_HOST}:${DATABASE_PORT}..."
-    sleep 10
+    sleep 5
 done
-
 echo ">> connected to MySQL database! <<"
+echo ">> Starting web server <<"
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /usr/local/tomcat/webapps/app.war
