ee48d83e5b994a0938936a1be2890d4ded97c746
Angeliqe <AngeliqeR@gmail.com>
2018-04-07 13:08:25 -0400

    auto incrementing id in case we need it.

--- database/02_data.sql
+++ database/02_data.sql
@@ -9,10 +9,26 @@ USE epidemics;
 # FLUSH PRIVILEGES;
 
 SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM `personType`;
+# This should be all the data we ever need for this table.
+# For this project, we will probably only use the patient row.
+INSERT INTO `personType` (`id`, `personTypeName`) VALUES (1, 'patient');
+INSERT INTO `personType` (`id`, `personTypeName`) VALUES (2, 'provider');
+
 /*
-DELETE FROM `login`;
-INSERT INTO `login` (`username`, `password`, `active`) VALUES ('april', 'beiter', 1);
-INSERT INTO `login` (`username`, `password`, `active`) VALUES ('jordan', 'rimmer', 1);
-INSERT INTO `login` (`username`, `password`, `active`) VALUES ('shanon', 'johnson', 0);
+select * from personType;
++----+----------------+
+| id | personTypeName |
++----+----------------+
+|  1 | patient        |
+|  2 | provider       |
++----+----------------+
 */
+
+DELETE FROM `person`;
+# This is a sample patient. I only filled in the necessary columns.
+# The other columns are nice, but won't affect the features of the final product.
+INSERT INTO `person` (`id`, `username`, `active`) VALUES (1, 'patient');
+
 SET SQL_SAFE_UPDATES = 1;
