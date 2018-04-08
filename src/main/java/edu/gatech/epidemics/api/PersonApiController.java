b7c6a26995675bb089b843e820104195bbe8cbd8
Angeliqe <AngeliqeR@gmail.com>
2018-04-07 20:09:07 -0400

    Removed unused classes. Person API complete.

--- src/main/java/edu/gatech/epidemics/api/PersonApiController.java
+++ src/main/java/edu/gatech/epidemics/api/PersonApiController.java
@@ -1,12 +1,17 @@
 package edu.gatech.epidemics.api;
 
 import edu.gatech.epidemics.entities.Person;
-import edu.gatech.epidemics.entities.PersonType;
 import edu.gatech.epidemics.service.PersonService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,30 +22,24 @@ public class PersonApiController {
     @Autowired
     private PersonService personService;
     
-    @GetMapping(value = "/api/person")
+    @GetMapping(value = "/api/persons")
     public List<Person> get() {
         return personService.findAll();
     }
     
-    @GetMapping(value = "/api/person/addExample")
-    public String addExample() {
-        Person person = new Person();
-        person.setActive(Boolean.TRUE);
-        person.setAddressLine1("Address Line1");
-        person.setAddressLine2("Address Line2");
-        person.setCity("Atlanta");
-        person.setContactEmail("email@test.com");
-        person.setContactFax("555-555-5556");
-        person.setContactPhone("555-555-5555");
-        person.setFirstName("John");
-        person.setLastName("Doe");
-        person.setMiddleName("M.");
-        person.setPassword("test");
-        person.setPersonType(PersonType.PATIENT);
-        person.setState("Georgia");
-        person.setUsername("john");
-        person.setZip("12345");
+    @GetMapping(value = "/api/persons/{id}")
+    public Person get(@PathVariable int id) {
+        Optional<Person> person = personService.findById(id);
+        return person.get();
+    }
+    
+    @RequestMapping(value = "/api/persons", params = "username")
+    public List<Person> findByUsername(@RequestParam("username") String username) {
+        return personService.findByUsername(username);
+    }
+    
+    @PostMapping(path = "/api/persons", consumes = "application/json", produces = "application/json")
+    public void addPerson(@RequestBody Person person) {
         personService.add(person);
-        return "added";
     }
 }
--- src/main/java/edu/gatech/epidemics/service/PersonService.java
+++ src/main/java/edu/gatech/epidemics/service/PersonService.java
@@ -4,6 +4,7 @@ import edu.gatech.epidemics.dao.PersonRepository;
 import edu.gatech.epidemics.entities.Person;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,14 @@ public class PersonService {
         List<Person> persons = new ArrayList<>();
         personRepository.findAll().forEach(persons::add);
         return persons;
+    }
+    
+    public Optional<Person> findById(Integer id) {
+        return personRepository.findById(id);
+    }
+    
+    public List<Person> findByUsername(String username) {
+        return personRepository.findByUsername(username);
     }
     
     public void add(Person person) {
