3bdc77d16d8add3f2cc4bf8bbaa0e9727fcaeb80
atalati6 <atalati6@gatech.edu>
2018-04-09 20:07:03 -0400

    add thymeleaf fragments and layouts

--- src/main/java/edu/gatech/epidemics/controller/HomeController.java
+++ src/main/java/edu/gatech/epidemics/controller/HomeController.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.*;
  */
 @Controller
 public class HomeController {
-    @PostMapping(value = "/")
-    public final String post(Login loginInput) {
-        System.out.println("HTTP POST: \"/api/login/\" : " + loginInput.toString());
-        if (loginInput.getUsername().equals("aarsh") && loginInput.getPassword().equals("test")) {
-            return "home";
-        } else {
-            return "redirect:/";
-        }
-    }
+//    @PostMapping(value = "/")
+//    public final String post(Login loginInput) {
+//        System.out.println("HTTP POST: \"/api/login/\" : " + loginInput.toString());
+//        if (loginInput.getUsername().equals("aarsh") && loginInput.getPassword().equals("test")) {
+//            return "home";
+//        } else {
+//            return "redirect:/";
+//        }
+//    }
 
     @GetMapping(value = "/")
     public String get() {
