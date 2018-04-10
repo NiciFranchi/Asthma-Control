84a235c3514bc39d68ecb461bf4fc0097de0733c
atalati6 <atalati6@gatech.edu>
2018-04-09 20:29:34 -0400

    fix 404 err for static resources

--- src/main/java/edu/gatech/epidemics/MvcConfig.java
+++ src/main/java/edu/gatech/epidemics/MvcConfig.java
@@ -9,8 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/META-INF/resources/",
+                "classpath:/resources/",
+                "classpath:/static/",
+                "classpath:/public/"
+            );
+        }
     }
 }
